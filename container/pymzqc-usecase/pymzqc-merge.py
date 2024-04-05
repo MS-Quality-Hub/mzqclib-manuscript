c63587c396d3da7992ef6165fbaf8cd6211a7253
mwalzer <walzer@ebi.ac.uk>
2024-04-05 15:46:38 +0000

    moved example scripts to container folder and updated the workflow readme merged merge scripts

--- container/pymzqc-usecase/pymzqc-merge.py
+++ container/pymzqc-usecase/pymzqc-merge.py
@@ -1,66 +1,103 @@
 #!/usr/local/bin/python
-from collections import defaultdict
-import os
+import logging
+import pandas as pd
 from mzqc import MZQCFile as qc
 import click
-import logging
 
-CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+def print_help():
+    """
+    Print the help of the tool
+    :return:
+    """
+    ctx = click.get_current_context()
+    click.echo(ctx.get_help())
+    ctx.exit()
 
-@click.command(short_help='A simple mzQC file merger using pymzqc assuming file metadata is compatible. ')
+def dedupe(file, mas=False, mif=False):
+    for r in file.runQualities:
+        # pd.Series(r.qualityMetrics).drop_duplicates(keep='first', inplace=False)  # no exact matches anyway
+        dupes = pd.Series([m.accession for m in r.qualityMetrics]).duplicated() 
+        # N.B. value diff for the dupes might be prudent here
+        for i in pd.Series(r.qualityMetrics)[dupes].index[::-1]:  # keep first, inplace
+            r.qualityMetrics.pop(i)
+        if mas:
+            dupes = pd.Series([m.location for m in r.metadata.inputFiles]).duplicated()
+            for i in pd.Series(r.metadata.inputFiles)[dupes].index[::-1]:  # keep first, inplace
+                r.metadata.inputFiles.pop(i)
+        if mif:
+            dupes = pd.Series([m.accession for m in r.metadata.analysisSoftware]).duplicated()
+            for i in pd.Series(r.metadata.analysisSoftware)[dupes].index[::-1]:  # keep first, inplace
+                r.metadata.analysisSoftware.pop(i)
+    return file
+
+def merge_single_run_files(file_1,file_2,ignore_location=False):
+    """
+    merge run quality objects if from the same run
+    stop if there are more than one runQualities in either file - we don't cover that here
+    runs are considerd the same if location, name, and format match
+    returns both files back, 
+        - first is the merger result or the first input if they dont match, 
+        - second is always second input
+    """
+    logging.debug(file_1.runQualities[0].metadata)
+    logging.debug(file_2.runQualities[0].metadata)
+
+    if (file_1.runQualities[0].metadata == file_2.runQualities[0].metadata):
+        # then just_merge the qualityMetrics
+        file_1.runQualities[0].qualityMetrics.extend(file_2.runQualities[0].qualityMetrics) 
+        dedupe(file_1)
+        logging.debug("dedupe same metadata")
+    elif file_1.runQualities[0].metadata.label == file_2.runQualities[0].metadata.label:
+        # now we need merge inputfiles, analysissoftware and qualitymetrics
+        file_1.runQualities[0].qualityMetrics.extend(file_2.runQualities[0].qualityMetrics) 
+        file_1.runQualities[0].metadata.analysisSoftware.extend(file_2.runQualities[0].metadata.analysisSoftware)
+        file_1.runQualities[0].metadata.inputFiles.extend(file_2.runQualities[0].metadata.inputFiles)
+        dedupe(file_1)
+        logging.debug("dedupe same label")
+    else:
+        file_1.runQualities.append(file_2.runQualities[0])
+        logging.debug("merely append runs")
+    return file_1
+
+def match_and_merge_multi_run_files(file_1,file_2):
+    pass
+
+def match_and_merge_sets_files(file_1,file_2):
+    pass
+
+@click.version_option('v1BETA')
+@click.command(short_help='A simple mzQC file merger using pymzqc assuming file metadata is compatible. mzQC files will be merged, where possible runs matched and metrics combined.')
 @click.argument('mzqc_input', nargs=-1, type=click.Path(exists=True,readable=True, dir_okay=False) )  # help="The mzqc files to merge"
 @click.argument('mzqc_output', type=click.Path(writable=True, dir_okay=False) )  # help="The output path for the resulting mzqc"
 @click.option('--log', type=click.Choice(['debug', 'info', 'warn'], case_sensitive=False),
-	default='warn', show_default=True,
-	required=False, help="Log detail level. (verbosity: debug>info>warn)")
-def simple_mzqc_merger(mzqc_input, mzqc_output, log):
-	"""
-	A simple mzQC file merger using pymzqc assuming file metadata is compatible. 
-	"""
-	# set loglevel - switch to match-case for py3.10+
-	lev = {'debug': logging.DEBUG,
-		'info': logging.INFO,
-		'warn': logging.WARN }
-	logging.basicConfig(format='%(levelname)s:%(message)s', level=lev[log])
+    default='warn', show_default=True,
+    required=False, help="Log detail level. (verbosity: debug>info>warn)")
+def merge_mzqc_files(mzqc_output, mzqc_input, log):
+    # set loglevel - switch to match-case for py3.10+
+    lev = {'debug': logging.DEBUG,
+     'info': logging.INFO,
+     'warn': logging.WARN }
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=lev[log])
 
-	try:
-		jmzqcs = dict()
-		for f in [f for f in mzqc_input if "jmzqc" in f ]:
-			with open(f, "r") as mzqcfile:
-				mzqcobj = qc.JsonSerialisable.FromJson(mzqcfile)
-				jmzqcs[os.path.splitext(os.path.splitext(f)[0])[0]]=mzqcobj
-		rmzqcs = dict()
-		for f in [f for f in mzqc_input if "rmzqc" in f ]:
-			with open(f, "r") as mzqcfile:
-				mzqcobj = qc.JsonSerialisable.FromJson(mzqcfile)
-				rmzqcs[os.path.splitext(os.path.splitext(f)[0])[0]]=mzqcobj
-		pmzqcs = dict()
-		for f in [f for f in mzqc_input if "pymzqc" in f ]:
-			with open(f, "r") as mzqcfile:
-				mzqcobj = qc.JsonSerialisable.FromJson(mzqcfile)
-				pmzqcs[os.path.splitext(os.path.splitext(f)[0])[0]]=mzqcobj
-		other = dict()
-		for f in [f for f in mzqc_input if not("pymzqc" in f) and not("jmzqc" in f) and not("rmzqc" in f)]:
-			with open(f, "r") as mzqcfile:
-				mzqcobj = qc.JsonSerialisable.FromJson(mzqcfile)
-				other[os.path.splitext(f)[0]]=mzqcobj
-		click.echo("Found {n} mzqc from jmzqc, \n{m} mzqc from rmzqc, \n{l} mzqc from pymzqc, \nand {i} mzqc from undetermined sources.".format(n=len(jmzqcs), m=len(rmzqcs), l=len(pmzqcs), i=len(other)))
-	except Exception as e:
-		click.echo(e)
-		print_help()
+    to_merge = list()
+    for fn in mzqc_input:
+        with open(fn, "r") as file:
+            to_merge.append(qc.JsonSerialisable.FromJson(file))
 
-	cv = qc.ControlledVocabulary(name="PSI-MS", uri="https://github.com/HUPO-PSI/psi-ms-CV/releases/download/v4.1.130/psi-ms.obo", version="v4.1.130")
-	mergedmzqc = qc.MzQcFile(version="1.0.0", 
-					description="Demo mzQC merged from dirfferent qc metric calculators", 
-					contactName="mwalzer", 
-					contactAddress="https://github.com/MS-Quality-Hub/mzqclib-manuscript", 
-					runQualities=[*jmzqcs.values(), *rmzqcs.values(), *pmzqcs.values(), *other.values()], 
-					controlledVocabularies=[cv]) 
+    if len(to_merge) < 2:
+        raise IndexError("Need at least 2 mzQC files to merge!")
+ 
+    if any([len(file.runQualities) > 1 for file in to_merge]):
+        raise NotImplementedError("Functionality not covered for files that contain multiple runs. Try a different function!")
+    else:
+        merged = next(iter(to_merge))
+        for file_right in to_merge[1::]:
+            merged = merge_single_run_files(merged,file_right,True)
 
-	with open(os.path.join(mzqc_output), "w") as file:
-		file.write(qc.JsonSerialisable.ToJson(mergedmzqc, readability=1))
-
-	click.echo("Files merged. Thank you for doing QC!")
+    with open(mzqc_output, "w") as file:
+        file.write(qc.JsonSerialisable.ToJson(merged, readability=1))
+    
+    click.echo("Files merged. Thank you for doing QC!")
 
 if __name__ == '__main__':
-	simple_mzqc_merger()
+    merge_mzqc_files()
